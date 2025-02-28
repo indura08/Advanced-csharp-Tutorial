b7bb84e989deee6ecb1ec4c2a3549a475f4309d8
indura08 <induraperera3@gmail.com>
2025-03-01 01:22:28 +0530

    initial commit to add delagtes practice project - club membership registration project

---
+++ Delegates/Mini-project-to-practice-delegates/ClubMembershipApplicationPracticeProject/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplicationPracticeProject.Model
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string EmailAddress { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "A password is must!")]
+        public string Password { get; set; } = null!;
+        public DateTime DateofBirth { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string PostCode { get; set; } = null!;
+
+    }
+}
---
+++ Delegates/Mini-project-to-practice-delegates/ClubMembershipApplicationPracticeProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubMembershipApplicationPracticeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

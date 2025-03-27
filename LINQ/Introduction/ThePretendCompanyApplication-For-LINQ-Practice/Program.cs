9db2daec430751804b09ff24df0c8ee16055f365
indura08 <induraperera3@gmail.com>
2025-03-27 20:55:21 +0530

    Project related to lesson LINQ-Queries (not completed only started)

--- LINQ/Introduction/ThePretendCompanyApplication-For-LINQ-Practice/Program.cs
+++ LINQ/Introduction/ThePretendCompanyApplication-For-LINQ-Practice/Program.cs
@@ -21,7 +21,32 @@ namespace ThePretendCompanyApplication_For_LINQ_Practice
                 Console.WriteLine();
             }
 
-            Console.ReadKey();
+            //Console.ReadKey();
+
+            //dan methna api krla tiynne apima extension method ekk hdgena eka use krnwa flter krnna enmployee or departments,
+            //but with LINQ mewage apita one krna filter extension methods hdlama thiynwa apita ewa use krnna puluwan
+            //example ekk widiyt pahatha linq query eka blnna
+
+            List<Employee> employeList2 = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartment();
+
+            var resultList = from emp in employeList2
+                             join dept in departmentList
+                             on emp.DepartmentId equals dept.Id
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 Department = dept.LongName
+                             };
+
+            foreach (var emp in resultList)
+            {
+                Console.WriteLine(emp.FirstName);
+                Console.WriteLine(emp.Department);
+
+                Console.WriteLine();
+            }
         
         }
     }
---
+++ LINQ/Queries/LINQExample_1/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace LINQExample_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeLIst = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartment();
+
+            //mke ithuru tika balnna 9:59:47n nwattuwe- code tika ghngnna hariytm me wlewatm tune krla
+        }
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; } = null!;
+            public string LastName { get; set; } = null!;
+            public decimal Annualsalary { get; set; }
+            public bool IsManager { get; set; }
+            public int DepartmentId { get; set; }
+        }
+
+        public class Department
+        {
+            public int Id { get; set; }
+            public string ShortName { get; set; } = null!;
+            public string LongName { get; set; } = null!;
+
+        }
+
+        public static class Data
+        {
+            public static List<Employee> GetEmployees()
+            {
+                List<Employee> employees = new List<Employee>();
+
+                Employee employee = new Employee
+                {
+                    Id = 1,
+                    FirstName = "Indura",
+                    LastName = "perera",
+                    Annualsalary = 220000.00m,
+                    IsManager = false,
+                    DepartmentId = 1
+                };
+
+                employees.Add(employee);
+
+                employee = new Employee
+                {
+                    Id = 2,
+                    FirstName = "Rashmi",
+                    LastName = "perera",
+                    Annualsalary = 20000.00m,
+                    IsManager = false,
+                    DepartmentId = 2
+                };
+
+                employees.Add(employee);
+
+                employee = new Employee
+                {
+                    Id = 3,
+                    FirstName = "Eliana",
+                    LastName = "Vivienne",
+                    Annualsalary = 60000.00m,
+                    IsManager = true,
+                    DepartmentId = 2
+                };
+
+                employees.Add(employee);
+
+                employee = new Employee
+                {
+                    Id = 4,
+                    FirstName = "Melina",
+                    LastName = "De silva",
+                    Annualsalary = 40000.00m,
+                    IsManager = true,
+                    DepartmentId = 3
+                };
+
+                employees.Add(employee);
+
+                return employees;
+            }
+
+            public static List<Department> GetDepartment()
+            {
+                List<Department> departments = new List<Department>();
+
+                Department department = new Department
+                {
+                    Id = 1,
+                    ShortName = "IT",
+                    LongName = "Information Technology"
+                };
+
+                departments.Add(department);
+
+                department = new Department
+                {
+                    Id = 2,
+                    ShortName = "HR",
+                    LongName = "Human Resource"
+                };
+
+                departments.Add(department);
+
+                department = new Department
+                {
+                    Id = 1,
+                    ShortName = "FN",
+                    LongName = "Finance"
+                };
+
+                departments.Add(department);
+
+                return departments;
+            }
+        }
+    }
+}
